19a856e0bedecbf030c5522af7c4e0669cf13ca1
Akshay Dongare <akshaydongare1797@gmail.com>
2019-03-02 16:38:59 +0000

    gitpod changes

--- src/main/java/com/tresk/controller/QuestionsController.java
+++ src/main/java/com/tresk/controller/QuestionsController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.tresk.service.BasicInfoService;
+import 
 import com.tresk.service.QuestionsService;
 import com.tresk.util.DynamicController;
 
@@ -16,12 +15,12 @@ public class QuestionsController {
 
 	@Autowired
 	QuestionsService service;
-	
+
 	@RequestMapping(value="/question/{serviceUrl}",method=RequestMethod.POST)
 	public String classDynamicController(@PathVariable String serviceUrl,@RequestBody String req) {
-		
+
 		DynamicController<QuestionsService> dynamicController = new DynamicController<>(service);
 		return dynamicController.serve(serviceUrl,req);
-		
+
 	}
 }
